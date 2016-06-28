96f969ac42a0f8de9c9326d352b126715a0bb8d1
furcon <furconit@gmail.com>
2016-06-28 19:15:00 -0400

    fix order of rows read in map

--- a2/q1.cc
+++ a2/q1.cc
@@ -8,16 +8,16 @@
 using namespace std;
 int main(int argc, char** argv) {
     if(argc != 2) {
-        printf("usage:\t ./search <maze-file.txt>\n"); 
+        printf("usage:\t ./search <maze-file.txt>\n");
         exit(-1);
     }
 
     char *fname = argv[1];
     std::ifstream infile(fname);
     std::string line;
-    
+
     char map[25][25];
-    int row = 0;
+    int row = 24;
     while (std::getline(infile, line)) { //will run 25 times
         char* characters = (char *)line.c_str();
         for (int col = 0; col < 25; col++) {
@@ -28,13 +28,14 @@ int main(int argc, char** argv) {
                 map[row][col] = characters[col];
             }
         }
-        row++;
+        row--;
     }
 
     for (int i = 0; i < 25; i++) {
         for (int j = 0; j < 25; j++) {
             cout << map[i][j];
         }
+        cout << endl;
     }
 
     exit(0);
