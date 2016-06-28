5da8664f2de9df2a8b3230ed43a1b25f2bad9f64
Aishwarya Pathak <pathak.isha07@gmail.com>
2016-06-28 19:12:27 -0400

    a2q1: file input

--- a2/q1.cc
+++ a2/q1.cc
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fstream>
+#include <iostream>
+
 using namespace std;
 int main(int argc, char** argv) {
     if(argc != 2) {
@@ -10,19 +12,31 @@ int main(int argc, char** argv) {
         exit(-1);
     }
 
-    char *fname = argc[1];
+    char *fname = argv[1];
     std::ifstream infile(fname);
     std::string line;
-    std::string map[25][25] = {0};
-    while (std::getline(infile, line)) {
-        std::istringstream iss(line);
-        
-        if (!(iss >> a >> b)) { break; } // error
+    
+    char map[25][25];
+    int row = 0;
+    while (std::getline(infile, line)) { //will run 25 times
+        char* characters = (char *)line.c_str();
+        for (int col = 0; col < 25; col++) {
+            if (!characters[col]) {
+                cout << "Error: invalid file" << endl;
+                exit(-1);
+            } else {
+                map[row][col] = characters[col];
+            }
+        }
+        row++;
+    }
 
-        // process pair (a,b)
-        // }
+    for (int i = 0; i < 25; i++) {
+        for (int j = 0; j < 25; j++) {
+            cout << map[i][j];
+        }
+    }
 
-
-
+    exit(0);
 }
 
